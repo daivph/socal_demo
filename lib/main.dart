4c7819b64a8999b7a3aac3d1a47dc965dbf45814
Mhossain028 <meraj.hossain028@gmail.com>
2023-03-03 01:25:09 +0600

    template setup

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:social_demo_app/on_boarding_screen/on_boarding_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const OnBoardingScreen(),
+    );
+  }
+}
